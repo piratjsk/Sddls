60638073bca6637bad0889397da912ba34b0618c
Adam Poloczek <piratjsk@gmail.com>
2018-06-21 19:01:46 +0200

    started implementing ProtectedPig

--- src/main/java/net/piratjsk/sddls/mount/ProtectedPig.java
+++ src/main/java/net/piratjsk/sddls/mount/ProtectedPig.java
@@ -1,18 +1,27 @@
 package net.piratjsk.sddls.mount;
 
 import net.piratjsk.sddls.saddle.SignedSaddle;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Pig;
+import org.bukkit.inventory.ItemStack;
 
 public class ProtectedPig extends GenericMount {
 
-    public ProtectedPig(Pig pig) {
+    private final SignedSaddle saddle;
+
+    public ProtectedPig(final Pig pig) {
         super(pig);
+        if(pig.hasSaddle())
+            this.saddle = SignedSaddle.fromItemStack(new ItemStack(Material.SADDLE));
+        else
+            this.saddle = SignedSaddle.fromItemStack(null);
+
     }
 
     @Override
     public SignedSaddle getSaddle() {
-        return null;
+        return this.saddle;
     }
 
     @Override
